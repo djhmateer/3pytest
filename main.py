7e2489239b083af9dc0bb2aa34699c48044e8d3d
Dave Mateer <davemateer@gmail.com>
2022-03-16 16:05:35 +0000

    auto

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ print('Your IP is {0}'.format(response.json()['origin']))
 x = requests.get('https://w3schools.com/python/demopage.htm')
 print(x.text)
 
-# Get Python Interpreter version
 # 3.10.2 with python3.10 main.py
 # 3.9.10 when run with python3.9 
 # 3.6.9 when run with python3 
+print("Python intepreter version:")
 print(sys.version)
